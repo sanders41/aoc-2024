169e11d36b5ba34f20dd0523c8fb4deb1d50ddc5
Paul Sanders <paul@paulsanders.dev>
2024-12-01 13:07:39 -0500

    Prep for day 2

--- src/year_2024/day2/mod.rs
+++ src/year_2024/day2/mod.rs
@@ -1,7 +1,85 @@
+use std::{
+    fs::File,
+    io::{BufReader, Lines},
+};
+
+use anyhow::Result;
+
+use crate::{
+    utils::{build_data_file_path, read_lines},
+    Day, Year,
+};
+
 pub fn puzzle1() {
-    println!("No implemented");
+    let file_path = build_data_file_path(&Year::Year2024, &Day::Day1, "data.txt").unwrap();
+    let lines = read_lines(file_path).unwrap();
+    let result = calculate_part_one(lines).unwrap();
+
+    println!("{result}");
 }
 
 pub fn puzzle2() {
-    println!("No implemented");
+    let file_path = build_data_file_path(&Year::Year2024, &Day::Day1, "data.txt").unwrap();
+    let lines = read_lines(file_path).unwrap();
+    let result = calculate_part_two(lines).unwrap();
+
+    println!("{result}");
+}
+
+fn calculate_part_one(lines: Lines<BufReader<File>>) -> Result<usize> {
+    let mut total = 0;
+
+    for line in lines.map_while(Result::ok) {
+        // TODO: implementaion
+    }
+
+    Ok(total)
+}
+
+fn calculate_part_two(lines: Lines<BufReader<File>>) -> Result<usize> {
+    let mut total = 0;
+
+    for line in lines.map_while(Result::ok) {
+        // TODO: implementaion
+    }
+
+    Ok(total)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        fs::{create_dir_all, File},
+        io::prelude::*,
+    };
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_part_one() {
+        let base = tempdir().unwrap().path().to_path_buf();
+        create_dir_all(&base).unwrap();
+        let file_path = base.join("data.txt");
+        let mut file = File::create(&file_path).unwrap();
+        let data = "";
+        file.write_all(data.as_bytes()).unwrap();
+        let lines = read_lines(file_path).unwrap();
+        let result = calculate_part_one(lines).unwrap();
+
+        assert_eq!(result, 0); // NOTE: placeholder
+    }
+
+    #[test]
+    fn test_part_two() {
+        let base = tempdir().unwrap().path().to_path_buf();
+        create_dir_all(&base).unwrap();
+        let file_path = base.join("data.txt");
+        let mut file = File::create(&file_path).unwrap();
+        let data = "";
+        file.write_all(data.as_bytes()).unwrap();
+        let lines = read_lines(file_path).unwrap();
+        let result = calculate_part_two(lines).unwrap();
+
+        assert_eq!(result, 0); // NOTE: placeholder
+    }
 }
