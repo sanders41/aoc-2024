a3cfc1f6ca88747e25aea57b8953d5616853ace3
Paul Sanders <paul@paulsanders.dev>
2024-12-03 10:35:02 -0500

    Add day 3 solutions

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,46 +1,73 @@
-use std::{
-    fs::File,
-    io::{BufReader, Lines},
-};
+use std::fs::read_to_string;
 
 use anyhow::Result;
+use regex::Regex;
 
-use crate::{
-    utils::{build_data_file_path, read_lines},
-    Day,
-};
+use crate::{utils::build_data_file_path, Day};
 
 pub fn puzzle1() {
-    let file_path = build_data_file_path(&Day::Day2, "data.txt").unwrap();
-    let lines = read_lines(file_path).unwrap();
-    let result = calculate_part_one(lines).unwrap();
+    let file_path = build_data_file_path(&Day::Day3, "data.txt").unwrap();
+    let data = read_to_string(file_path).unwrap();
+    let result = calculate_part_one(&data).unwrap();
 
     println!("{result}");
 }
 
 pub fn puzzle2() {
-    let file_path = build_data_file_path(&Day::Day2, "data.txt").unwrap();
-    let lines = read_lines(file_path).unwrap();
-    let result = calculate_part_two(lines).unwrap();
+    let file_path = build_data_file_path(&Day::Day3, "data.txt").unwrap();
+    let data = read_to_string(file_path).unwrap();
+    let result = calculate_part_two(&data).unwrap();
 
     println!("{result}");
 }
 
-fn calculate_part_one(lines: Lines<BufReader<File>>) -> Result<usize> {
+fn calculate_part_one(data: &str) -> Result<usize> {
     let mut total = 0;
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
 
-    for line in lines.map_while(Result::ok) {
-        // TODO: implementation
+    for values in re.find_iter(data) {
+        let values_str = values.as_str();
+        let stripped = values_str.replace("mul(", "").replace(')', "");
+        let nums = stripped
+            .split(",")
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+        total += nums[0] * nums[1];
     }
 
     Ok(total)
 }
 
-fn calculate_part_two(lines: Lines<BufReader<File>>) -> Result<usize> {
+fn calculate_part_two(data: &str) -> Result<usize> {
     let mut total = 0;
+    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
 
-    for line in lines.map_while(Result::ok) {
-        // TODO: implementation
+    let parts = data.split("don't");
+    for (index, part) in parts.enumerate() {
+        if index == 0 {
+            for values in re.find_iter(&part) {
+                let values_str = values.as_str();
+                let stripped = values_str.replace("mul(", "").replace(')', "");
+                let nums = stripped
+                    .split(",")
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>();
+                total += nums[0] * nums[1];
+            }
+        } else {
+            if let Some(start) = part.find("do") {
+                for values in re.find_iter(&part[start..]) {
+                    let values_str = values.as_str();
+                    let stripped = values_str.replace("mul(", "").replace(')', "");
+                    let nums = stripped
+                        .split(",")
+                        .map(|s| s.parse::<usize>().unwrap())
+                        .collect::<Vec<usize>>();
+                    println!("{:?}", nums);
+                    total += nums[0] * nums[1];
+                }
+            }
+        }
     }
 
     Ok(total)
@@ -49,36 +76,20 @@ fn calculate_part_two(lines: Lines<BufReader<File>>) -> Result<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::{
-        fs::{create_dir_all, File},
-        io::prelude::*,
-    };
-    use tempfile::tempdir;
 
     #[test]
     fn test_part_one() {
-        let base = tempdir().unwrap().path().to_path_buf();
-        create_dir_all(&base).unwrap();
-        let file_path = base.join("data.txt");
-        let mut file = File::create(&file_path).unwrap();
-        let data = r#""#;
-        file.write_all(data.as_bytes()).unwrap();
-        let lines = read_lines(file_path).unwrap();
-        let result = calculate_part_one(lines).unwrap();
+        let data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let result = calculate_part_one(data).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 161);
     }
 
+    #[test]
     fn test_part_two() {
-        let base = tempdir().unwrap().path().to_path_buf();
-        create_dir_all(&base).unwrap();
-        let file_path = base.join("data.txt");
-        let mut file = File::create(&file_path).unwrap();
-        let data = r#""#;
-        file.write_all(data.as_bytes()).unwrap();
-        let lines = read_lines(file_path).unwrap();
-        let result = calculate_part_two(lines).unwrap();
+        let data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        let result = calculate_part_two(data).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 48);
     }
 }
