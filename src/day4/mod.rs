af95a7049fd3ea1ab015bd0702da1ea98c5f9fb3
Paul Sanders <paul@paulsanders.dev>
2024-12-04 10:56:08 -0500

    Add day 4 solutions

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -3,7 +3,7 @@ use std::{
     io::{BufReader, Lines},
 };
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 
 use crate::{
     utils::{build_data_file_path, read_lines},
@@ -28,9 +28,47 @@ pub fn puzzle2() {
 
 fn calculate_part_one(lines: Lines<BufReader<File>>) -> Result<usize> {
     let mut total = 0;
+    let word_chars = ['X', 'M', 'A', 'S'];
+    let grid = build_vec(lines).unwrap();
+    let directions = vec![
+        (0, 1),
+        (0, -1),
+        (1, 0),
+        (-1, 0),
+        (1, 1),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+    ];
 
-    for line in lines.map_while(Result::ok) {
-        // TODO: implemenation
+    for row in 0..grid.len() {
+        for column in 0..grid[row].len() {
+            if grid[row][column] != 'X' {
+                continue;
+            }
+
+            for (x, y) in &directions {
+                let mut found = true;
+                for (i, _) in word_chars.iter().enumerate() {
+                    let row_check = row as isize + i as isize * x;
+                    let column_check = column as isize + i as isize * y;
+
+                    if row_check < 0
+                        || column_check < 0
+                        || row_check >= grid.len() as isize
+                        || column_check >= grid.len() as isize
+                        || grid[row_check as usize][column_check as usize] != word_chars[i]
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if found {
+                    total += 1;
+                }
+            }
+        }
     }
 
     Ok(total)
@@ -38,12 +76,45 @@ fn calculate_part_one(lines: Lines<BufReader<File>>) -> Result<usize> {
 
 fn calculate_part_two(lines: Lines<BufReader<File>>) -> Result<usize> {
     let mut total = 0;
+    let grid = build_vec(lines).unwrap();
 
-    for line in lines.map_while(Result::ok) {
-        // TODO: implemenation
+    for row in 1..grid.len() - 1 {
+        for column in 1..grid[row].len() - 1 {
+            if grid[row][column] == 'A' {
+                let top_left_to_bottom_right =
+                    grid[row - 1][column - 1] == 'M' && grid[row + 1][column + 1] == 'S';
+
+                let top_right_to_bottom_left =
+                    grid[row - 1][column + 1] == 'M' && grid[row + 1][column - 1] == 'S';
+
+                let bottom_left_to_top_right =
+                    grid[row + 1][column - 1] == 'M' && grid[row - 1][column + 1] == 'S';
+
+                let bottom_right_to_top_left =
+                    grid[row + 1][column + 1] == 'M' && grid[row - 1][column - 1] == 'S';
+
+                if top_left_to_bottom_right && top_right_to_bottom_left
+                    || top_left_to_bottom_right && bottom_left_to_top_right
+                    || bottom_right_to_top_left && top_right_to_bottom_left
+                    || bottom_right_to_top_left && bottom_left_to_top_right
+                {
+                    total += 1;
+                }
+            }
+        }
     }
 
     Ok(total)
+}
+
+fn build_vec(lines: Lines<BufReader<File>>) -> Result<Vec<Vec<char>>> {
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    for line in lines.map_while(Result::ok) {
+        let columns = line.chars().collect::<Vec<char>>();
+        grid.push(columns);
+    }
+
+    Ok(grid)
 }
 
 #[cfg(test)]
@@ -61,12 +132,21 @@ mod tests {
         create_dir_all(&base).unwrap();
         let file_path = base.join("data.txt");
         let mut file = File::create(&file_path).unwrap();
-        let data = r#""#;
+        let data = r#"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"#;
         file.write_all(data.as_bytes()).unwrap();
         let lines = read_lines(file_path).unwrap();
         let result = calculate_part_one(lines).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 18);
     }
 
     #[test]
@@ -75,11 +155,20 @@ mod tests {
         create_dir_all(&base).unwrap();
         let file_path = base.join("data.txt");
         let mut file = File::create(&file_path).unwrap();
-        let data = r#""#;
+        let data = r#"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"#;
         file.write_all(data.as_bytes()).unwrap();
         let lines = read_lines(file_path).unwrap();
         let result = calculate_part_two(lines).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 9);
     }
 }
